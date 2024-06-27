6c5eae7bf04c78fabd20954871f2987f6994f16c
smith-steve <Steve.A.Dore@outlook.com>
2024-06-27 07:23:49 -0700

    push

--- src/LamdaSESTest/obj/Release/net8.0/linux-x64/LamdaSESTest.AssemblyInfo.cs
+++ src/LamdaSESTest/obj/Release/net8.0/linux-x64/LamdaSESTest.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("LamdaSESTest")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fdb05e8cd1c4eb46e17379d1b1c5bd5b4ca613a")]
 [assembly: System.Reflection.AssemblyProductAttribute("LamdaSESTest")]
